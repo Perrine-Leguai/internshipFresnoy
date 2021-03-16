5e1144b97ad4d3eef2966bf8165ba5de1d5d6da9
Perrine-Leguai <perrine.leguai@gmail.com>
2021-03-16 17:10:59 +0100

    pu rien

--- src/app/_model/title.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Title{
-  name : string;
-  author_firstName : string;
-  author_lastName : string;
-  year : string;
-}
--- src/app/_model/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User{
-  id : number;
-  url : string;
-  username: string;
-  first_name : string;
-  last_name : string;
-}
