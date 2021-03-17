f8c32e91d8e0866ffde760bb77689093aeb1f6f6
Perrine-Leguai <perrine.leguai@gmail.com>
2021-03-17 16:12:03 +0100

    french artwork form working

---
+++ interface/_controller/studentViewController.php
@@ -0,0 +1,8 @@
+<?php
+
+    require_once('../_service/UserService.php');
+    require_once('../_service/StudentService.php');
+    require_once('../presentation/communHtml.php');
+    
+    html('Etudiant.e.s');
+?>
