1d0d140184d76a9d20722872d169ec90e0152fcc
Perrine-Leguai <perrine.leguai@gmail.com>
2021-03-17 09:09:08 +0100

    v1 base structure

---
+++ interface/interface/InterfaceDao.php
@@ -0,0 +1,11 @@
+<?php
+
+    interface InterfaceDao {
+        public function create(Object $objet);
+        // public function delete(Int $id);
+      
+        public function searchAll();
+        public function searchBy(Int $id); 
+        // public function update(Object $objet, int $idOdbjectToModify);
+    }
+?>
